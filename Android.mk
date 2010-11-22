a3eb5d9b10d9f9c5edb646e2e28296784999fbc6
Kolja Dummann <k.dummann@gmail.com>
2010-11-22 20:49:23 +0100

    initial commit

---
+++ Android.mk
@@ -0,0 +1,21 @@
+
+ifeq ($(strip $(BOARD_USES_NMEA_GPS)),true)
+
+  LOCAL_PATH := $(call my-dir)
+
+  include $(CLEAR_VARS)
+
+  LOCAL_SRC_FILES := \
+    gps_nmea.c
+
+  LOCAL_MODULE := libgps-nmea
+
+  LOCAL_SHARED_LIBRARIES := \
+    libutils \
+    libcutils \
+    libdl \
+    libc
+
+  include $(BUILD_SHARED_LIBRARY)
+
+endif
