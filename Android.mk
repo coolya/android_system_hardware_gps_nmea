1091e1f233c20a9f0a794ad83962fc2c22340962
Kolja Dummann <k.dummann@gmail.com>
2010-11-23 22:13:29 +0100

    fix makefile

--- Android.mk
+++ Android.mk
@@ -15,7 +15,10 @@ ifeq ($(strip $(BOARD_USES_NMEA_GPS)),true)
     libcutils \
     libdl \
     libc
-
+  LOCAL_PRELINK_MODULE := false
+  LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
   include $(BUILD_SHARED_LIBRARY)
+
+
 
 endif
